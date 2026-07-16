d90ddccaa97074d6fe32b1d6acf3f03273371387
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-07-16 23:59:47 +0530

    added admin function

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
-from
+from fastapi import APIRouter, Depends, HTTPException,Header
+from app.services.dbmodel import User
 
 auth_router = APIRouter()
+
+ADMIN_KEY = os.getenv("ADMIN_KEY")
+
+def check_admin(admin_key):
+    if admin_key == ADMIN_KEY:
+        return True
+    else:
+        raise HTTPException(status_code=401,detail="Admin key is invalid Unauthorized")
 
